cf0cb05242f876c68e5bb9f3f5915444bbd57788
Elena <lady.aleck98@icloud.com>
2022-12-15 21:03:05 +0300

    Еще коммит

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string[] arrayInput = new string[] { "...", "..", "....", ".", "....." };
+
+            string[] newArray = 
+                (from ELEMENT in arrayInput 
+                 where ELEMENT.Length <= 3 select ELEMENT)
+                .ToArray();
+
+
+
+   
